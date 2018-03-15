0560c9e2f95240af7fbd8f6d93e293163a8963f2
jc428499 <levi.smith@my.jcu.edu.au>
2018-03-16 09:29:46 +1000

    refactored functions Added frequency

--- odd_name.py
+++ odd_name.py
@@ -1,12 +1,13 @@
 def main():
     run = True
     name = input(print("What is your name?:  "))
+    frequency = input(print("How often would you like a letter to be printed?: "))
     get_name(name, run)
-    show_name(name)
+    show_name(name, frequency)
 
 
-def show_name(name):
-    for a in range(0, 4, 2):
+def show_name(name, frequency):
+    for a in range(0, 4, frequency):
         print(name[a])
 
 
