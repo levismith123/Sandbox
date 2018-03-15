ff301bc0f23e915e8efc0066ccd1a2c2d8732860
jc428499 <levi.smith@my.jcu.edu.au>
2018-03-16 09:07:50 +1000

    Prints git hub files refactored

--- odd_name.py
+++ odd_name.py
@@ -1,9 +1,18 @@
-run = True
-name = input(print("What is your name?:  "))
-while run == True:
-    if name == '':
-        print("Name cannot be blank")
-    else:
-        run = False
-for a in range (0,4,2):
-    print(name[a])
+def main():
+    run = True
+    name = input(print("What is your name?:  "))
+    get_name(name, run)
+    show_name(name)
+
+
+def show_name(name):
+    for a in range(0, 4, 2):
+        print(name[a])
+
+
+def get_name(name, run):
+    while run == True:
+        if name == '':
+            print("Name cannot be blank")
+        else:
+            run = False
